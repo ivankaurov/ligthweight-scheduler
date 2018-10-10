3045d99a0ac957c2360bb4a1e8805cb69329b2c9
Ivan Kaurov <ivan.kaurov@gmail.com>
2018-10-10 10:39:10 +0300

    IJobManager interface

---
+++ src/Lightweight.Scheduler.Abstractions/IJobManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Lightweight.Scheduler.Abstractions
+{
+    using System.Threading.Tasks;
+
+    public interface IJobManager<TJobKey>
+    {
+        Task ScheduleJob(IIdentifier<TJobKey> jobId, IJobMetadata jobMetadata);
+
+        Task<bool> RemoveJob(IIdentifier<TJobKey> jobId);
+
+        Task PauseJob(IIdentifier<TJobKey> jobId);
+
+        Task ResumeJob(IIdentifier<TJobKey> jobId);
+
+        Task<IJobMetadata> GetJob(IIdentifier<TJobKey> jobId);
+    }
+}
