63c01f2001acebfc78ad863ae4796df76535dc8c
Ivan Kaurov <ikaurov@intermedia.net>
2020-03-23 13:01:01 +0300

    ValidateOptionsBase added to shared utils

---
+++ src/Utils/ValidateOptionsBase.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ValidateOptionsBase.cs" company="Intermedia">
+//   Copyright © Intermedia.net, Inc. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Lightweight.Scheduler.Utils
+{
+    using System.Collections.Generic;
+
+    using Microsoft.Extensions.Options;
+
+    public abstract class ValidateOptionsBase<TOptions> : IValidateOptions<TOptions>
+        where TOptions : class
+    {
+        public ValidateOptionsResult Validate(string name, TOptions options)
+        {
+            var errors = this.ValidateInternal(options).AsCollection();
+
+            return errors.Count > 0 ? ValidateOptionsResult.Fail(errors) : ValidateOptionsResult.Success;
+        }
+
+        protected abstract IEnumerable<string> ValidateInternal(TOptions options);
+    }
+}
