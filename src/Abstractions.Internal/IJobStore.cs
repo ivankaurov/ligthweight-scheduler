ba8555c08b7009bc08c8a02a8671d5c43c4ad4ad
Ivan Kaurov <ikaurov@intermedia.net>
2020-03-23 14:07:06 +0300

    IJobStore interface introduced

---
+++ src/Abstractions.Internal/IJobStore.cs
@@ -0,0 +1,20 @@
+﻿namespace Lightweight.Scheduler.Abstractions.Internal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IJobStore<TJobId, in TNodeId, TVersion>
+    {
+        IAsyncEnumerable<IJobDescriptor<TJobId, TVersion>> GetJobsForExecution(int count, CancellationToken cancellationToken);
+
+        Task Put(IJobDescriptor<TJobId, TVersion> jobDescriptor, CancellationToken cancellationToken);
+
+        Task StartExclusiveExecution(IVersionedId<TJobId, TVersion> jobId, TNodeId nodeId, TimeSpan? nextExecution, CancellationToken cancellationToken);
+
+        Task StartExecution(IVersionedId<TJobId, TVersion> jobId, TimeSpan? nextExecution, CancellationToken cancellationToken);
+
+        Task CompleteExecution(IJobDescriptor<TJobId, TVersion> jobDescriptor, CancellationToken cancellationToken);
+    }
+}
