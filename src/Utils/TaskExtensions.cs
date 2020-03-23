fb1962209e76602ab141865612ba09bf57f98bc7
Ivan Kaurov <ikaurov@intermedia.net>
2020-03-23 15:27:27 +0300

    FireAndForget task extensions added

---
+++ src/Utils/TaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TaskExtensions.cs" company="Intermedia">
+//   Copyright © Intermedia.net, Inc. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Lightweight.Scheduler.Utils
+{
+    using System.Threading.Tasks;
+
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+        }
+    }
+}
---
+++ src/Utils/TaskUtils.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TaskUtils.cs" company="Intermedia">
+//   Copyright © Intermedia.net, Inc. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Lightweight.Scheduler.Utils
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class TaskUtils
+    {
+        public static void FireAndForget(Func<Task> action)
+        {
+            ForceAsync(action).Forget();
+        }
+
+        public static async Task ForceAsync(Func<Task> action)
+        {
+            await Task.Yield();
+            await action().ConfigureAwait(false);
+        }
+    }
+}
